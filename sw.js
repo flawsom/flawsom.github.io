c9eeac8c18f0decb203d894df1702a53acf2f3a6
Siba4738 <64182228+Siba4738@users.noreply.github.com>
2023-01-01 12:39:53 +0530

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,38 @@
+const soundCloud = document.querySelector('.sound-cloud')
+const off = document.querySelector('#off')
+const on = document.querySelector('#on')
+const myAudio = document.querySelector('#myAudio')
+
+off.addEventListener('click', () => soundTrack('off'))
+on.addEventListener('click', () => soundTrack('on'))
+
+const soundTrack = (soundState) => {
+  if (soundState === 'off') {
+    on.style.display = 'block'
+    off.style.display = 'none'
+    soundCloud.style.color = '#08fdd8'
+    myAudio.play()
+  } else if (soundState === 'on') {
+    on.style.display = 'none'
+    off.style.display = 'block'
+    soundCloud.style.color = '#f50057'
+    myAudio.pause()
+  }
+}
+
+// Play music functionality
+
+const btnBars = document.querySelector('.bars')
+const btnTimes = document.querySelector('.times')
+const SideNav = document.querySelector('.aside')
+
+btnBars.addEventListener('click', () => myFunc('open'))
+btnTimes.addEventListener('click', () => myFunc('close'))
+
+
 var CACHE_NAME = "spectrum-cache-v1";
 var urlsToCache = ["/mousetrail/", "/mousetrail/index.html"];
+
 
 self.addEventListener("install", function (event) {
 	event.waitUntil(
